cd0bad716f24cf4ab46fbed5953eb20f7b1c2411
Iamneo <techadmin@iamneo.ai>
2023-08-05 11:30:20 +0000

    Backup Commit

--- reactapp/reactapp/src/index.js
+++ reactapp/reactapp/src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import stopwatch from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <stopwatch />
   </React.StrictMode>,
   document.getElementById('root')
 );
